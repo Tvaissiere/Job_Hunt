de861768811712d3f5db37085b114aa6af63b985
TVaissiere <TVaissiere@outlook.com>
2026-03-16 00:47:13 +0000

    basic functionality acheived - a few kinks

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+# TODO: Make it open new tab instead of window
 import webbrowser
 
-webbrowser.open("https://google.com")
+# TODO: List most popular employers
+companies = ["Monzo",]
+job_role = ""
+
+
+def main(companies, job_role):
+    job_role = input("What job role are you searching for? ")
+    for i in range(len(companies)):
+        webbrowser.open("https://www.google.com/search?q="+job_role+" "+companies[i])
+
+main(companies, job_role)
