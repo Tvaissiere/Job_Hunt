97b3b8c6db78c4dd7840f054f7748eb740169141
TVaissiere <TVaissiere@outlook.com>
2026-03-16 00:50:51 +0000

    got it to open in new tabs instead of windows

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-# TODO: Make it open new tab instead of window
 import webbrowser
 
 # TODO: List most popular employers
-companies = ["Monzo",]
+companies = ["Monzo", "Lloyds",]
 job_role = ""
 
 
 def main(companies, job_role):
     job_role = input("What job role are you searching for? ")
     for i in range(len(companies)):
-        webbrowser.open("https://www.google.com/search?q="+job_role+" "+companies[i])
+        webbrowser.open_new_tab("https://www.google.com/search?q="+job_role+" "+companies[i])
 
 main(companies, job_role)
